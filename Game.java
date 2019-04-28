583cbd6acbe6d081baf6f06c38acb1f5cf5801ad
Danielscott1229 <48811011+Danielscott1229@users.noreply.github.com>
2019-04-28 18:58:39 -0400

    Update Game.java

--- Game.java
+++ Game.java
@@ -26,7 +26,10 @@ public class Game extends JPanel {
     ArrayList<ShipShot> shipShotToBeDeleted = new ArrayList<ShipShot>();
     int alienSpeed = 1;
     int score = 0;
-    String state = "Playing";
+    int level = 1;
+    int timer = -1;
+    int lives = 3;
+    String state = "start_menu";
 
     //This method returns the score
     private int getScore() {
@@ -55,14 +58,14 @@ public class Game extends JPanel {
     }
 
     //this method creates a ship shot object and adds it to the shipShotArray
-    void shipShoot(int x, int y){
+    void shipShoot(int x, int y) {
         shipShotArray.add(new ShipShot(this, x, y));
     }
 
     //this method spawns all aliens in their starting positions
     private void spawnAliens() {
-        for(int i=100;i<401;i+=40) {
-            for (int ii=40;ii<201;ii+=40) {
+        for (int i = 100; i < 401; i += 40) {
+            for (int ii = 40; ii < 201; ii += 40) {
                 alienArray.add(new Alien(this, i, ii));
             }
         }
@@ -71,20 +74,26 @@ public class Game extends JPanel {
     //this method calls the "step" method of every object in the game. The step method is a method that executes every
     //single "frame" of the game.
     private void step() {
-        for(Alien a : this.alienArray) {
+        for (Alien a : this.alienArray) {
             a.step();
         }
-        for(ShipShot b : this.shipShotArray) {
+        for (ShipShot b : this.shipShotArray) {
             b.step();
         }
         ship.step();
     }
 
+    private void resetAlienPositions() {
+        for (Alien a : this.alienArray) {
+            a.x = a.initial_x;
+            a.y = a.initial_y;
+        }
+    }
     //This method checks for collisions between different objects
     private void checkCollisions() {
         //This part of the code checks for collisions between all of the ShipShot objects and all of the Alien objects
-        for(ShipShot b : this.shipShotArray) {
-            for(Alien a : this.alienArray) {
+        for (ShipShot b : this.shipShotArray) {
+            for (Alien a : this.alienArray) {
                 if (a.getBounds().intersects(b.getBounds())) {
                     //because Java gets mad when we try to delete an item from a list we are currently iterating through,
                     //we will instead add all colliding shipShots and Aliens into trash arrays, and we will delete them
@@ -97,20 +106,30 @@ public class Game extends JPanel {
         }
         //This part of the code checks for collisions between the ship and all of the alien objects. If there is a
         //collision, the game ends.
-        for(Alien a : this.alienArray) {
+        for (Alien a : this.alienArray) {
             if (a.getBounds().intersects(ship.getBounds())) {
                 //This part of the code clears all of the aliens, ship shots, and the player object from the screen
-                for(Alien b : this.alienArray) {
-                    alienToBeDeleted.add(b);
-                }
-                for(ShipShot c : this.shipShotArray) {
+                for (ShipShot c : this.shipShotArray) {
                     shipShotToBeDeleted.add(c);
                 }
-                state = "Game Over";
+                if (lives > 0) {
+                    if (state == "playing") {
+                        lives -= 1;
+                        state = "try_again";
+                        timer = 60;
+                    }
+                }
+                if (lives == 0) {
+                    state = "game_over";
+                    lives = 3;
+                    for (Alien b : this.alienArray) {
+                        alienToBeDeleted.add(b);
+                    }
+                }
                 //Put score code here!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
                 //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
             }
-        //This part of the code removes objects we couldn't safely remove before from our "To Be Deleted" Lists
+            //This part of the code removes objects we couldn't safely remove before from our "To Be Deleted" Lists
         }
         alienArray.removeAll(alienToBeDeleted);
         shipShotArray.removeAll(shipShotToBeDeleted);
@@ -119,17 +138,18 @@ public class Game extends JPanel {
     //This method checks how many Aliens are remaining, and adjusts the alienSpeed variable accordingly
     private void checkAlienCount() {
         int alienCount = alienArray.size();
-        if (alienCount < 10) {
-            alienSpeed = 4;
+        if ((alienCount == 0) && (state == "playing")) {
+            level += 1;
+            spawnAliens();
         }
-        else if (alienCount < 20) {
-            alienSpeed = 3;
+        if (alienCount < 15) {
+            alienSpeed = 3 + (level - 1);
         }
         else if (alienCount < 30) {
-            alienSpeed = 2;
+            alienSpeed = 2 + (level - 1);
         }
-        else{
-            alienSpeed = 1;
+        else {
+            alienSpeed = 1 + (level - 1);
         }
     }
 
@@ -139,45 +159,79 @@ public class Game extends JPanel {
         super.paint(g);
         Graphics2D g2d = (Graphics2D) g;
         g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        for(Alien a : this.alienArray) {
-            a.paint(g2d);
+        switch (state) {
+            case "playing":
+                for (Alien a : this.alienArray) {
+                    a.paint(g2d);
+                }
+                for (ShipShot b : this.shipShotArray) {
+                    b.paint(g2d);
+                }
+                ship.paint(g2d);
+                g2d.setColor(Color.GRAY);
+                g2d.setFont(new Font("Verdana", Font.BOLD, 30));
+                g2d.drawString(Integer.toString(lives), 30, 40);
+                g2d.drawString(Integer.toString(score), 300, 40);
+                break;
+            case "try_again":
+                g2d.setColor(Color.GRAY);
+                g2d.setFont(new Font("Verdana", Font.BOLD, 60));
+                g2d.drawString("Try Again", 30, 300);
+                if (timer > 0) {
+                   timer -= 1;
+                }
+                else {
+                    resetAlienPositions();
+                    state = "playing";
+                }
+                break;
+            case "game_over":
+                g2d.setColor(Color.GRAY);
+                g2d.setFont(new Font("Verdana", Font.BOLD, 60));
+                g2d.drawString("GAME OVER", 30, 300);
+                g2d.setFont(new Font("Verdana", Font.BOLD, 40));
+                g2d.drawString("Press SPACE to try again", 30, 400);
+                break;
+            case "start_menu":
+                g2d.setColor(Color.GRAY);
+                g2d.setFont(new Font("Verdana", Font.BOLD, 60));
+                g2d.drawString("SPACE INVADERS", 30, 250);
+                g2d.setFont(new Font("Verdana", Font.BOLD, 20));
+                g2d.drawString("By: Daniel Scott and Miranda Thompson", 30, 300);
+                g2d.drawString("Press SPACE to begin", 30, 400);
+                break;
+            }
         }
-        for(ShipShot b : this.shipShotArray) {
-            b.paint(g2d);
+
+        //This method brings up a text box which tells the player they got a game over
+        public void gameOver () {
+            JOptionPane.showMessageDialog(this, "your score is: " + getScore(),
+                    "Game Over", JOptionPane.YES_NO_OPTION);
+            System.exit(ABORT);
         }
-        ship.paint(g2d);
 
-        g2d.setColor(Color.GRAY);
-        g2d.setFont(new Font("Verdana", Font.BOLD, 30));
-        g2d.drawString(String.valueOf(getScore()), 10, 30);
-        g2d.drawString(String.valueOf(shipShotArray.size()), 50, 30);
-    }
+        public void setupGame () {
+        score = 0;
+        state = "playing";
+        spawnAliens();
+        }
 
-    //This method brings up a text box which tells the player they got a game over
-    public void gameOver() {
-        JOptionPane.showMessageDialog(this, "your score is: " + getScore(),
-                "Game Over", JOptionPane.YES_NO_OPTION);
-        System.exit(ABORT);
-    }
-
-    //the main method
-    public static void main(String[] args) throws InterruptedException {
-        //creates the window for the game, as well as a new Game object
-        JFrame frame = new JFrame("Space Invaders");
-        Game game = new Game();
-        frame.add(game);
-        frame.setSize(600, 600);
-        frame.setVisible(true);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        //spawns aliens in intial location
-        game.spawnAliens();
-        //this is the main loop, which will repeat for as long as the game is running
-        while (true) {
-            game.step();
-            game.repaint();
-            game.checkAlienCount();
-            game.checkCollisions();
-            Thread.sleep(10);
+        //the main method
+        public static void main (String[]args) throws InterruptedException {
+            //creates the window for the game, as well as a new Game object
+            JFrame frame = new JFrame("Space Invaders");
+            Game game = new Game();
+            frame.add(game);
+            frame.setSize(600, 600);
+            frame.setVisible(true);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            //this is the main loop, which will repeat for as long as the game is running
+            while (true) {
+                 game.step();
+                 game.repaint();
+                 game.checkAlienCount();
+                 game.checkCollisions();
+                 Thread.sleep(10);
+            }
         }
     }
-}
