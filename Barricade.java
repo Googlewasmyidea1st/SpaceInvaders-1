e5816f59a91330eb213b3d2a985ff7c5f2c9929a
Danielscott1229 <48811011+Danielscott1229@users.noreply.github.com>
2019-05-13 11:49:04 -0400

    Update Barricade.java

--- Barricade.java
+++ Barricade.java
@@ -2,9 +2,11 @@ package com.spaceinvaders;
 
 import java.awt.*;
 import java.awt.Image;
+
 import javax.swing.ImageIcon;
 
 public class Barricade {
+    //initialize variables
     int x = 0;
     int y = 0;
     private static final int WIDTH = 9;
@@ -12,6 +14,7 @@ public class Barricade {
     private Game game;
     private Image spr_barricade;
 
+    //constructor
     public Barricade(Game game, int initx, int inity) {
         this.game = game;
         x = initx;
@@ -19,16 +22,19 @@ public class Barricade {
         loadImage();
     }
 
+    //loadImage() loads Barricade's sprites to memory
     private void loadImage() {
         ImageIcon barricade = new ImageIcon("src/sprites/Barricade.png");
         spr_barricade = barricade.getImage();
     }
 
+    //paint() draws the Barricade to the screen
     public void paint(Graphics2D g) {
         Graphics2D g2d = (Graphics2D) g;
         g2d.drawImage(spr_barricade, x, y, null);
     }
 
+    //getBounds() is used for collision detection
     public Rectangle getBounds() {
         return new Rectangle(x, y, WIDTH, HEIGHT);
     }
